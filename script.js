d15c2715fdcb1c737f4a937c3ef778a0120ea93b
Ajayn02 <ajayaj2002nov@gmail.com>
2024-08-13 16:27:43 +0530

    new

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 const searchBtn = document.querySelector("#searchbtn")
 const searchInput = document.querySelector("#inpt")
 
-const keyWords = [`egg`, `lamb`, `chicken`, `noodles`, `cake`, `fish`, `seafood`, `milk`, `veg`, `pork`, `duck`, `lemon`, `yogurt`, `honey`,`salad`,`pizza`,`pickle`,`beef`]
+// firstpage data
+let mainCards=``
+let maincontent=fetch("./maincontent.json").then(val=>val.json()).then(item=>item.meals.forEach((val)=>{
+  mainCards+=`
+      <div class="col-lg-3 col-md-6 col-sm-10 col-10 col1">
+      <div class="card cd1" style="width: 18rem;" onclick="getRecipie(event)">
+  <img src="${val.strMealThumb}" class="card-img-top" alt="...">
+  <div class="card-body id="cd">
+    <h5 class="card-title">${val.strMeal}</h5>
+    <p class="card-text">${val.strTags}</p>
+    <p class="card-text">${val.strArea}</p>
+    <a href="" target="_blank
+" class="btn  " id="ViewR" onclick="getRecipe(event,${val.idMeal})" data-bs-toggle="modal" data-bs-target="#exampleModal">View Recipe</a>
+  </div>
+</div>
+</div>
+  `
+
+  m1.innerHTML=mainCards
+})
+)
+
+
+
+
+
+
+
+// inpt suggestions
+
+const keyWords = [`egg`, `lamb`, `chicken`, `noodles`, `cake`, `fish`, `seafood`, `milk`, `veg`, `pork`, `duck`, `lemon`, `yogurt`, `honey`,`salad`,`pizza`,`pickle`,`beef`,`biryani`]
 
 const getSugg = (e) => {
   let suggetions = searchInput.value
@@ -73,21 +103,30 @@ const getUrl = (e) => {
 </div>
 `
       n1.innerHTML = a
-      main1.style.display = "none";
-      footer.style.display = "block";
+      mainhead.style.display="none";
+      m1.style.display="none";
+      header.style.display="none"
+      // main1.style.display = "none";
+      // footer.style.display = "block";
     })
 
     ).catch(err => {
-      main1.style.display = "none";
-      let e = `<h1 class="text-center"> Sorry Not found!!</h1>`
-      n1.innerHTML = e
+      // main1.style.display = "none";
+      // mainh1.style.display="none";
+      mainhead.style.display="none";
+      header.style.display="block"
+      let e = `<h1 class="text-center"><b> Sorry Not found!</b></h1>`
+      header.innerHTML = e
     })
 
   }
   else {
-    main1.style.display = "none";
-    let nothing = `<h2 class="text-center"><b>Enter Your Favorite Dish!</b></h2>`
-    n1.innerHTML = nothing
+    // main1.style.display = "none";
+    // mainh1.style.display="none";
+    mainhead.style.display="none";
+    header.style.display="block"
+    let nothing = `<h1 class="text-center"><b>Enter Your Favorite Dish!</b></h1>`
+    header.innerHTML = nothing
   }
 }
 
